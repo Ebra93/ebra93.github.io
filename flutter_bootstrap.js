853745769c3aef3eb87d2a1cb79665333f4a7667
ebrahemaj <ebrahem.aljamous@alumni.fh-aachen.de>
2025-08-08 12:34:33 +0200

    update

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2420569222"
+    serviceWorkerVersion: "2855734659"
   }
 });
